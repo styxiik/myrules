7a90053114d6cd9e717e0d50d0cba47f478da9e0
styxiik <84225473+styxiik@users.noreply.github.com>
2022-06-03 18:28:33 +0800

    update

---
+++ QX/AddWhiteList3.js
@@ -0,0 +1,21 @@
+const url = "whitelist3";
+const method = "POST";
+const headers = {"Field": "test-header-param"};
+const data = {"info": "abc"};
+const myRequest = {
+    url: url,
+    method: method, // Optional, default GET.
+    headers: headers, // Optional.
+    body: JSON.stringify(data) // Optional.
+};
+
+$task.fetch(myRequest).then(response => {
+    // response.statusCode, response.headers, response.body
+    console.log(response.body);
+    $notify("白名单", "添加", response.body); // Success!
+    $done();
+}, reason => {
+    // reason.error
+    $notify("白名单", "错误", reason.error); // Error!
+    $done();
+});
